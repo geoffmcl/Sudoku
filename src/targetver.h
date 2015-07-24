bdac53e2010a1b69775b36e43776df52cce7c3d3
Geoff R. McLane <ubuntu@geoffair.info>
2015-07-24 13:41:40 +0200

    Only if WIN32

--- src/targetver.h
+++ src/targetver.h
@@ -15,11 +15,12 @@
 #define _TARGETVER_H_
 // #pragma once // prefer the above as croos platform compatible
 
-
+#ifdef WIN32
 // Including SDKDDKVer.h defines the highest available Windows platform.
 // If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
 // set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 #include <SDKDDKVer.h>
+#endif
 
 // This should be the SAME as the VERSION resource
 #define APP_VERSION "0.9.0.2"
