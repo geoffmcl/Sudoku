590ba976a785be5b001a34321ff09a82ea081c53
Geoff McLane <ubuntu@geoffair.info>
2015-07-26 13:04:00 +0200

    Use pthread for unix

--- src/Sudo_Thread.cxx
+++ src/Sudo_Thread.cxx
@@ -3,15 +3,15 @@
 #include "Sudoku.hxx"
 #ifdef WIN32    // include 'process.h'
 #include <process.h>
-#endif  // WIN32 process.h
+#else // !WIN32 // include <pthread.h>
+#include <pthread.h>
+#endif  // WIN32 process.h/pthread.h
 #include "Sudo_Time.hxx"
 
 static PABOX2 pb_copy = 0;
 static int got_solution = 0;
 static int in_thread = 0;
 
-#ifdef WIN32    // window THREAD handling
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
 static HANDLE hThread = 0;
 static unsigned threadID = 0;
 //static DWORD dwMilliseconds = INFINITE;
@@ -27,6 +27,9 @@ int get_solution_value( int row, int col )
         return pb_copy->line[row].val[col];
     return 0;
 }
+
+#ifdef WIN32    // window THREAD handling
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void wait_thread_exit()
 {
@@ -92,13 +95,70 @@ bool StartThread()
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 #else // !#ifdef WIN32 - TODO: Need alterative THREAD handling
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-int get_solution_value( int row, int col )
+/* this function is run by the second thread */
+void *ThreadFunc(void *vp)
 {
-    if (!in_thread && got_solution && pb_copy) {
-        return pb_copy->line[row].val[col];
-    }
+    Timer tm;
+    in_thread++;
+    tm.start();
+    set_solution_valid(0);
+    //sprtf( "In second thread...\n" );
+    got_solution = Get_Solution(pb_copy, true);
+    //sprtf( "Thread exit... %d\n", got_solution );
+    //HANDLE hand = hThread;
+    //hThread = 0;
+    //if (hand)
+    //    CloseHandle(hand);
+    tm.stop();
+    thread_time = tm.getElapsedTime();
+    in_thread--;
+    //if (use_endthreadex)
+    //    _endthreadex( 0 );
     return 0;
+
 }
+
+bool StartThread()
+{
+    bool bret = false;
+    if (in_thread)
+        return bret;
+    if (!is_curr_box_valid_for_paint())
+        return bret;
+    int cnt = Get_Spot_Count();
+    if (!cnt || (cnt >= 81))
+        return bret;
+    PABOX2 pb = get_curr_box();
+    // sprtf( "Creating second thread... %d spots\n", cnt );
+    if (pb_copy) {
+        int row, col;
+        for (row = 0; row < 9; row++) {
+            for (col = 0; col < 9; col++) {
+                pb_copy->line[row].val[col] = pb->line[row].val[col];
+            }
+        }
+    } else 
+        pb_copy = Copy_Box2(pb);
+
+    // Create the second thread.
+    // hThread = (HANDLE)_beginthreadex( NULL, 0, &ThreadFunc, NULL, 0, &threadID );
+    /* create a second thread which executes inc_x(&x) */
+    int x = 0;
+    if(pthread_create(&threadID, NULL, ThreadFunc, &x)) 
+    {
+        fprintf(stderr, "Error creating thread\n");
+        return false;
+
+    }
+    //if (hThread) {
+        //sprtf( "Got second thread...\n" );
+        bret = true;
+    //} else {
+    //    sprtf( "Second thread failed...%d - %s\n", errno, strerror(errno) );
+    //}
+    return bret;
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 #endif // #ifdef WIN32 y/n - THREAD handling
 
