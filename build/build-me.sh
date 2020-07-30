e31d043e1cd7085a6205bea67d9a9d14b1553667
Geoff R. McLane <ubuntu@geoffair.info>
2020-07-30 16:05:07 +0200

    Add opts DEBUG VERBOSE to build-me.sh

--- build/build-me.sh
+++ build/build-me.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#< build-me.sh for Sudoku - 20150724
+#< build-me.sh for Sudoku - 20200730 -20150724
 
 BN=`basename $0`
 
@@ -10,19 +10,54 @@ if [ -f "$TMPLOG" ]; then
     fi
     mv $TMPLOG $TMPLOG.bak
 fi
-echo ""
-echo "$BN: Some suggested cmake options to use for debug..."
-echo "  -DCMAKE_VERBOSE_MAKEFILE=TRUE - use a verbose Makefile good to see flags. switches, libraries, etc..."
-echo "  -DCMAKE_BUILD_TYPE=DEBUG - to add symbols for gdb use (add -g compiler switch)"
-echo "  Then run gdb with '\$ gdb --args ask prompt'"
-echo "  -DCMAKE_INSTALL_PREFIX:PATH=$HOME - to add a spcific install location"
-echo ""
+
+give_help()
+{
+    echo ""
+    echo "$BN: Some suggested options to use for debug..."
+    echo "  VERBOSE = Add -DCMAKE_VERBOSE_MAKEFILE=TRUE - set verbose Makefile to see flags. switches, libraries, etc..."
+    echo "  DEBUG   = -DCMAKE_BUILD_TYPE=DEBUG - to add symbols for gdb use (add -g compiler switch). Def=Release"
+    echo "  Then run gdb with '\$ gdb --args ask prompt'"
+    echo "  -DCMAKE_INSTALL_PREFIX:PATH=$HOME - to add a spcific install location"
+    echo ""
+    exit 1
+}
 
 TMPOPTS=""
-for arg in $@; do
-    TMPOPTS="$TMPOPTS $arg"
+TMPDBG=0
+TMPHLP=0
+
+#for arg in $@; do
+#    TMPOPTS="$TMPOPTS $arg"
+#done
+
+##############################################
+### ***** NOTE THIS INSTALL LOCATION ***** ###
+### Change to suit your taste, environment ###
+##############################################
+: ${TMPOPTS:="-DCMAKE_INSTALL_PREFIX=\"${CMAKE_INSTALL_PREFIX:-$HOME}\""}
+#############################################
+# Use -DCMAKE_BUILD_TYPE=Debug to add gdb symbols
+# Use -DCMAKE_VERBOSE_MAKEFILE=ON
+
+# correctly quoted $@ is the default for shell for loops
+for arg; do
+    case "$arg" in
+       VERBOSE) TMPOPTS="$TMPOPTS -DCMAKE_VERBOSE_MAKEFILE=ON";;
+        DEBUG) TMPDBG=1;;
+        *) echo "Unknown argument '$arg'!"; TMPHLP=1;;
+    esac
 done
 
+if [ "$TMPHLP" = "1" ]; then
+    give_help
+fi
+
+if [ "$TMPDBG" = "1" ]; then
+    TMPOPTS="$TMPOPTS -DCMAKE_BUILD_TYPE=Debug"
+else
+    TMPOPTS="$TMPOPTS -DCMAKE_BUILD_TYPE=Release"
+fi
 
 echo "$BN: Doing 'cmake .. $TMPOPTS', output to $TMPLOG..."
 cmake .. $TMPOPTS >$TMPLOG 2>&1
